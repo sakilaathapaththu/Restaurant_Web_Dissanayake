791df86d77267e47c9985f52d6800f54eb9ab3df
Praveen Liyanage <111709030+PraveenLiyanage@users.noreply.github.com>
2025-08-27 00:25:09 +0530

    Home Routes Modify

---
+++ frontend/src/Pages/homepage.jsx
@@ -0,0 +1,24 @@
+// src/Pages/Home.jsx
+import React from "react";
+import HomepageNavbar from "../Components/Navbar/Homepagenavbar";
+import Homecarousel from "../Components/Home/Homecarousel";
+import Aboutsection from "../Components/Home/Aboutsection";
+import Featuredmanu from "../Components/Home/Featuredmanu";
+import Highlights from "../Components/Home/Highlights";
+import Simplecontact from "../Components/Home/Simplecontact";
+import Footer from "../Components/Home/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <HomepageNavbar />
+      <Homecarousel />
+      {/* 
+      <Aboutsection />
+      <Featuredmanu />
+      <Highlights />
+      <Simplecontact />
+      <Footer /> */}
+    </>
+  );
+}
