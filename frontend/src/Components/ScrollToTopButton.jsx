b0ab8a51190383594f3f698b9f9cfa658266ad31
Praveen Liyanage <111709030+PraveenLiyanage@users.noreply.github.com>
2025-08-31 21:43:30 +0530

    button position

--- frontend/src/Components/ScrollToTopButton.jsx
+++ frontend/src/Components/ScrollToTopButton.jsx
@@ -6,7 +6,7 @@ import { styled } from '@mui/material/styles';
 
 const StyledFab = styled(Fab)(({ theme }) => ({
 position: 'fixed',
-bottom: theme.spacing(13),
+bottom: theme.spacing(15),
 right: theme.spacing(3),
 backgroundColor: '#fda021',
 color: '#2c1000',
