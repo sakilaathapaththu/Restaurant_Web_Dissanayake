ef4fc9f375b27a12129b85f0afddd2c6b5f42e0b
Praveen Liyanage <111709030+PraveenLiyanage@users.noreply.github.com>
2025-09-06 21:43:55 +0530

    Remove spam controller

--- Backend/routes/inquiryRoutes.js
+++ Backend/routes/inquiryRoutes.js
@@ -2,8 +2,6 @@ const router = require("express").Router();
 const rateLimit = require("express-rate-limit");
 const inquiryCtrl = require("../controllers/inquiryController");
 
-router.use(rateLimit({ windowMs: 60 * 1000, max: 6 })); // simple spam control
-
 router.post("/", inquiryCtrl.create);
 router.get("/", inquiryCtrl.list);
 router.patch("/:id/status", inquiryCtrl.updateStatus);
